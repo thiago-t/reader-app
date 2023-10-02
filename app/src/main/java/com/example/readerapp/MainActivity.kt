391623039d72db4a3e69c3d62852cb3ba0a721ed
Thiago Toazza <thiagotoazza@outlook.com>
2023-10-02 20:42:21 -0300

    Setup basics of Hilt dependency injection

--- app/src/main/java/com/example/readerapp/MainActivity.kt
+++ app/src/main/java/com/example/readerapp/MainActivity.kt
@@ -11,13 +11,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.readerapp.ui.theme.ReaderAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ReaderAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
